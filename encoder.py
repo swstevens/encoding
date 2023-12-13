6ab824671ddac68f3b2947b1cf80cfc88de1480c
swstevens <sstevensw@gmail.com>
2023-12-12 22:02:23 -0800

    implement node class

--- encoder.py
+++ encoder.py
@@ -1,5 +1,22 @@
 import sys
 
+class Node():
+    def __init__(self, head):
+        self.head =  head
+        self.left = None
+        self.right = None
+    def __contains__(self, head):
+        if head == self.head:
+            return True
+        return (False if self.left is None else head in self.left) or (False if self.right is None else head in self.right)
+    def insert(self, head, left, right):
+        if self.head is None:
+            self.head, self.left, self.right = head, Node(left), Node(right)
+        elif self.head == head:
+            self.left, self.right = Node(left), Node(right)
+        else:
+            getattr(self, 'left' if self.left and head in self.left else 'right').insert(head, left, right)
+
 def main():
     count = {}
     if sys.argv[1] is not None:
